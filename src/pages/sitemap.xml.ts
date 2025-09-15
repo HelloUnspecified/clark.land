ca4179b4dd136ecaac0246f71e6e929620d554e3
Clark Sell <clark.sell@csell.net>
2025-09-15 17:09:33 -0500

    initial add

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,20 @@
+import type { APIRoute } from 'astro';
+
+export const GET: APIRoute = () => {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://clark.land/</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+</urlset>`;
+
+  return new Response(sitemap, {
+    headers: {
+      'Content-Type': 'application/xml',
+      'Cache-Control': 'public, max-age=3600',
+    },
+  });
+};
